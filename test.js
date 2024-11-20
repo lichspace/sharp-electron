2ff7ec647bb72953c46b0e9454178f1ef5938b45
tangentguo-win <tangentguo@tencent.com>
2024-11-20 18:43:43 +0800

    feat: electron bundles

---
+++ test.js
@@ -0,0 +1,3 @@
+const sharp = require('./bundle/index.js');
+console.log(sharp);
+sharp("C:\\Users\\tangentguo\\Downloads\\HT66_Ep21_CUT137_上色_001\\HT66_Ep21_CUT137_上色_001\\A\\A0010.png").metadata(console.log);
